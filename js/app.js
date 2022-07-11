e9d4ccf26e42ca6ae43fb13ba548d0ac92ce326e
jason606 <stonegee606@yahoo.com>
2022-07-11 21:03:44 +0000

    Mobile Nav

--- js/app.js
+++ js/app.js
@@ -9,9 +9,21 @@ const copyMenu = () => {
   const navPlace = document.querySelector(".off-canvas nav");
   navPlace.innerHTML = mainNav.innerHTML;
 
-  const topNav = document.querySelector(".header-top");
+  const topNav = document.querySelector(".header-top .wrapper");
   const topPlace = document.querySelector(".off-canvas .thetop-nav");
   topPlace.innerHTML = topNav.innerHTML;
 };
+
+
+const toggle = (e) => {
+    e.preventDefault();
+    submenu.forEach(item => item != this? item.closest('.has-child').classList.remove('.expand') : null);
+}
+
+const submenu = document.querySelectorAll(".has-child .icon-small");
+submenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+
+
 
 copyMenu();
